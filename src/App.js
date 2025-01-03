c8783139817a109bdb8e5d90f170cb1eb7e634cf
Amolina5 <amolina555@hotmail.com>
2025-01-03 14:31:45 -0600

    Catalog page added with img

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import CatalogPage from './components/CatalogPage';
 import Footer from './components/footer';
-import ArcadeMachine from './components/ArcadeMachine';
+//import ArcadeMachine from './components/ArcadeMachine';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Navbar></Navbar>
       <h1>Hello world from react</h1>
 
-      <ArcadeMachine></ArcadeMachine>
+      <CatalogPage></CatalogPage>
       
       <Footer></Footer>    
     </div>
