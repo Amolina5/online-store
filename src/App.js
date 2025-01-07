8c0b9b23d7180b7e2a95d76682bd671be8f3b340
Amolina5 <amolina555@hotmail.com>
2025-01-06 19:05:06 -0600

    assignment 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import AboutPage from './components/AboutPage';
 
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
-//import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 //import "bootstrap/dist/js/bootstrap.js";
 //import ArcadeMachine from './components/ArcadeMachine';
 
